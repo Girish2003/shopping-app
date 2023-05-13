c6b37593fd89e9d6deaa84f4185bf0f5679f15db
Girish2003 <cgirish2003@gmail.com>
2023-05-13 16:57:13 +0530

    first commit

---
+++ shopping-cart-react-main/src/products.js
@@ -0,0 +1,66 @@
+import product1 from "./assets/products/1.jpeg";
+import product2 from "./assets/products/66.jpeg";
+import product3 from "./assets/products/33.jpeg";
+import product4 from "./assets/products/44.jpeg";
+import product5 from "./assets/products/55.jpeg";
+import product6 from "./assets/products/10.jpeg";
+import product7 from "./assets/products/77.jpeg";
+import product8 from "./assets/products/88.jpeg";
+import product9 from "./assets/products/12.jpeg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Premium Back Skin",
+    price: 20.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Iphone Stand",
+    price: 35.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Premium BackCover",
+    price: 24.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Wired Charger",
+    price: 35.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Wireless Charger",
+    price: 50.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Camera Guard",
+    price: 16.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Selfie stick",
+    price: 22.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Leather Flip Cover",
+    price: 29.0,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "Screen Protector",
+    price: 25.0,
+    productImage: product9,
+  },
+];
